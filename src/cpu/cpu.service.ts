c2a88010d41dde175087283b3be80eb404337599
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-14 16:11:18 +0100

    working on PowerService

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+
+  computer(a: number, b: number) {
+    console.log('drawing 10 whats of power from powerService');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
+}
